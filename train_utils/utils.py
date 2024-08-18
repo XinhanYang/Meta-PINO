6fdd536b7ff9a27e3918f1f971797c099b5c9061
Xinhan Yang <xinhanyang@gatech.edu>
2024-08-18 14:33:58 -0400

    change save dir.

--- train_utils/utils.py
+++ train_utils/utils.py
@@ -154,7 +154,8 @@ def count_params(net):
 
 
 def save_checkpoint(path, name, model, optimizer=None):
-    ckpt_dir = 'checkpoints/%s/' % path
+    # ckpt_dir = 'checkpoints/%s/' % path
+    ckpt_dir = '%s/' % path
     if not os.path.exists(ckpt_dir):
         os.makedirs(ckpt_dir)
     try:
